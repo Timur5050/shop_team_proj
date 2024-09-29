42a8cd830db9851c288194c675bd75c96a3fab03
timur <stukantimur811@gmail.com>
2024-09-29 10:11:58 +0300

    Created temprorary view for products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from products.models import Generator, Battery, Inverter, PortablePowerStation
+from products.serializers import (
+    GeneratorSerializer,
+    BatterySerializer,
+    InverterSerializer,
+    PortablePowerStationSerializer
+)
+
+
+class ProductViewSet(ModelViewSet):
+    serializer_class = GeneratorSerializer
+    queryset = Generator.objects.all()
+
+
+class BatteryViewSet(ModelViewSet):
+    serializer_class = BatterySerializer
+    queryset = Battery.objects.all()
+
+
+class InverterViewSet(ModelViewSet):
+    serializer_class = InverterSerializer
+    queryset = Inverter.objects.all()
+
+
+class PortablePowerStationViewSet(ModelViewSet):
+    serializer_class = PortablePowerStationSerializer
+    queryset = PortablePowerStation.objects.all()
