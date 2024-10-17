f67e5bfaa3ae4fec3964ade4d211c4a4c101dd59
timur <stukantimur811@gmail.com>
2024-10-17 18:21:30 +0300

    Added new db

--- shop/settings.py
+++ shop/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -77,10 +78,27 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres.nhugjmxkpzizxcfwulrb',
+#         'PASSWORD': '107teamproject!',
+#         'HOST': 'aws-0-eu-central-1.pooler.supabase.com',  # Замість цього введіть свій хост
+#         'PORT': '5432',
+#     }
+# }
+from dotenv import load_dotenv
+
+load_dotenv()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
