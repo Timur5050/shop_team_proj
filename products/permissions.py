b4c7dac65fdf947dcaec0b504995ab843e4b37ea
timur <stukantimur811@gmail.com>
2024-10-23 18:51:26 +0300

    added image support for every product

--- products/permissions.py
+++ products/permissions.py
@@ -4,9 +4,7 @@ from rest_framework import permissions
 class ReadOnlyOrAdminPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
         return bool(
-            (
-                    request.method in permissions.SAFE_METHODS
-                    and request.user
-                    and request.user.is_authenticated)
+
+            request.method in permissions.SAFE_METHODS
             or request.user and request.user.is_staff
         )
